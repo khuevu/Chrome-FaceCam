79765e9b75054b8d667a8bb8c6ec3c37d07a25d1
Khue Vu <khuevu@gmail.com>
2012-10-10 12:02:01 +0800

    add auto monitor

--- demo.js
+++ demo.js
@@ -32,21 +32,37 @@ function getGeo() {
     navigator.geolocation.clearWatch(watchId);
 }
 
+function compareImage() {
+	//scale captured image to original image
+	var context2 = canvas2.getContext('2d');
+	context2.translate(canvas2.width / 2, canvas2.height / 2); 
+	context2.scale(canvas.width / canvas2.width, canvas.height / canvas.height);
+	//see how
+
+}
+
 function closedEnough(rect) {
-	if ($(video).width / 4 < rect[2] && $(video).height / 4 < rect[3]) {
+	console.log('video width' + $(video).width() + ' - ' + $(video).height());
+	if ($(video).width() / 4 < rect[2] && $(video).height() / 4 < rect[3]) {
 		return true;
 	
 	}
     return false;
 }
 
-function captureFaceImage(imageUrl, rect) {
-	var imgCtx = img.getContext('2d');
+function captureFaceImage(imageUrl, rect, canvas) {
+	
+	canvas.width = rect[2];
+	canvas.height = rect[3];
+	//img.width = rect[2];
+	//img.height = rect[3];
+	var imgCtx = canvas.getContext('2d');
 	var imageObj = new Image();
 	imageObj.onload = function() {
-			imgCtx.drawImage(imageObj, x, y, w, h, 0, 0, w, h);
+			imgCtx.drawImage(imageObj, rect[0], rect[1], rect[2], rect[3], 0, 0, rect[2], rect[3]);
 		}
 	imageObj.src = imageUrl;
+	
 }
 
 function tick() {
@@ -62,12 +78,23 @@ function tick() {
             if (coords[0]) {
                 coords = smoother.smooth(coords[0]);
                 console.log(coords);
-                
+                face = coords;
                 $(this).highlight(coords, "red");
-                
+               
                 if (closedEnough(coords)) {
                 	//capture the face image
+              		if (!faceDetected && faceSampled) {
+		            	var tmpCanvas = document.createElement('canvas');
+						tmpCanvas.width = video.videoWidth;
+						tmpCanvas.height = video.videoHeight;
+						var ctx = tmpCanvas.getContext('2d');
+						ctx.drawImage(video, 0, 0);
+						//img.src = canvas.toDataURL('image/webp');
+						captureFaceImage(tmpCanvas.toDataURL('image/webp'), coords, canvas2);
+						faceDetected = true;
+					}
                 }
+               
               
             }
         });
@@ -88,14 +115,20 @@ $.fn.highlight = function (rect, color) {
     }).appendTo("body");
 }
 
+var face; 
 var video;
 var img;
+var canvas;
+var canvas2;
 var smoother = new Smoother(0.85, [0, 0, 0, 0, 0]);
+var faceDetected = false;
+var faceSampled = false;
 
 function runVideo() {
     video = document.querySelector('#screenshot-stream');
     var button = document.querySelector('#screenshot-button');
-    var canvas = document.querySelector('#screenshot-canvas');
+    canvas = document.querySelector('#screenshot-canvas');
+    canvas2 = document.querySelector('#screenshot2-canvas');
     img = document.querySelector('#screenshot');
     var ctx = canvas.getContext('2d');
     var localMediaStream = null;
@@ -104,14 +137,22 @@ function runVideo() {
         setTimeout(function () {
             canvas.width = video.videoWidth;
             canvas.height = video.videoHeight;
+            canvas2.width = video.videoWidth;
+            canvas2.height = video.videoHeight;
             img.height = video.videoHeight;
             img.width = video.videoWidth;
         }, 50);
     }
 
     function snapshot() {
+    	var tmpCanvas = document.createElement('canvas');
+    	tmpCanvas.width = video.videoWidth;
+    	tmpCanvas.height = video.videoHeight;
+    	var ctx = tmpCanvas.getContext('2d');
         ctx.drawImage(video, 0, 0);
-        img.src = canvas.toDataURL('image/webp');
+        //img.src = canvas.toDataURL('image/webp');
+        captureFaceImage(tmpCanvas.toDataURL('image/webp'), face, canvas);
+        faceSampled = true;
     }
     button.addEventListener('click', function (e) {
         if (localMediaStream) {
